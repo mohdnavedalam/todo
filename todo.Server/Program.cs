a9748c5cf243c069ca143f08b3f4352949aff885
Mohd Naved Alam <alam00star@gmail.com>
2025-08-31 06:01:06 +0530

    swagger for production

--- todo.Server/Program.cs
+++ todo.Server/Program.cs
@@ -22,11 +22,14 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
