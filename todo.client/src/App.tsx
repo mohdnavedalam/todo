9b8162d9a0be48df986fd85478db38002ddc8f3b
Mohd Naved Alam <alam00star@gmail.com>
2025-08-31 17:56:22 +0530

    minor fixes

--- todo.client/src/App.tsx
+++ todo.client/src/App.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import axios from 'axios';
 import Todo from './Todo';
 
 function App() {
-    const [response, setResponse] = useState<any>(null);
-
     return (
         <>
             <Todo />
