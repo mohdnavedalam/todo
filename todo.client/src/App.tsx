1181d2cee9ee8330ec7dad386027956a106ca29f
Mohd Naved Alam <alam00star@gmail.com>
2025-08-31 17:50:21 +0530

    cleanup "test"

--- todo.client/src/App.tsx
+++ todo.client/src/App.tsx
@@ -6,26 +6,8 @@ import Todo from './todo';
 function App() {
     const [response, setResponse] = useState<any>(null);
 
-
-    const fetchData = async () => {
-        try {
-            const result = await axios.get('https://localhost:7015/api/Test');
-            setResponse(result.data);
-            console.log(result.data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                {response}
-            </h1>
             <Todo />
         </>
     );
