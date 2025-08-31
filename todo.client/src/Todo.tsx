8d2e27c2ee05737ec7c824a13f44e867ec671aac
Mohd Naved Alam <alam00star@gmail.com>
2025-08-31 18:08:05 +0530

    basic todo - no api

--- todo.client/src/Todo.tsx
+++ todo.client/src/Todo.tsx
@@ -1,21 +1,38 @@
+import { useState } from 'react';
 import './Todo.css';
 
 const Todo = () => {
-  return (
-    <div className="todo-container">
-        <h1 className="todo-title">Todo List</h1>
-        <div className="todo-input-group">
-            <input 
-                type="text" 
-                placeholder="Add a todo" 
-                className="todo-input"
-            />
-            <button className="todo-button">
-                Add
-            </button>
+    const [todos, setTodos] = useState<string[]>([]);
+    const handleAddTodo = () => {
+        // Logic to add a todo item
+        const newTodo = (document.querySelector('.todo-input') as HTMLInputElement).value;
+        if (newTodo.trim() !== '') {
+            setTodos([...todos, newTodo]);
+            (document.querySelector('.todo-input') as HTMLInputElement).value = '';
+        }
+    }
+    return (
+        <div className="todo-container">
+            <h1 className="todo-title">Todo List</h1>
+            <div className="todo-input-group">
+                <input
+                    type="text"
+                    placeholder="Add a todo"
+                    className="todo-input"
+                />
+                <button className="todo-button" onClick={handleAddTodo}>
+                    Add
+                </button>
+            </div>
+            <ul className="todo-list">
+                {todos.map((todo, index) => (
+                    <li key={index} className="todo-item">
+                        {todo}
+                    </li>
+                ))}
+            </ul>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Todo;
