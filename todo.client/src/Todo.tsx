825e8e457a8e0b3ff0ab9a12a9673db55cd16b67
Mohd Naved Alam <alam00star@gmail.com>
2025-09-06 07:27:51 +0530

    modifications on add and delete items logic | added and modified css classes

--- todo.client/src/Todo.tsx
+++ todo.client/src/Todo.tsx
@@ -3,13 +3,19 @@ import './Todo.css';
 
 const Todo = () => {
     const [todos, setTodos] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState<string>('');
     const handleAddTodo = () => {
         // Logic to add a todo item
-        const newTodo = (document.querySelector('.todo-input') as HTMLInputElement).value;
+        const newTodo = inputValue
         if (newTodo.trim() !== '') {
             setTodos([...todos, newTodo]);
-            (document.querySelector('.todo-input') as HTMLInputElement).value = '';
+            setInputValue('');
         }
+    }
+    const handleDeleteTodo = (index: number) => {
+        // Logic to delete a todo item
+        const newTodos = todos.filter((_, i) => i !== index);
+        setTodos(newTodos);
     }
     return (
         <div className="todo-container">
@@ -19,6 +25,8 @@ const Todo = () => {
                     type="text"
                     placeholder="Add a todo"
                     className="todo-input"
+                    onChange={e => setInputValue(e.target.value)}
+                    value={inputValue}
                 />
                 <button className="todo-button" onClick={handleAddTodo}>
                     Add
@@ -26,8 +34,19 @@ const Todo = () => {
             </div>
             <ul className="todo-list">
                 {todos.map((todo, index) => (
-                    <li key={index} className="todo-item">
-                        {todo}
+                    <li key={index} >
+                        <div className="todo-item">
+                            <div className='todo-card'>
+                                {todo}
+                            </div>
+                            <button
+                                className="todo-delete"
+                                onClick={() => {handleDeleteTodo(index)}}
+                                tabIndex={0}
+                            >
+                                Delete
+                            </button>
+                        </div>
                     </li>
                 ))}
             </ul>
